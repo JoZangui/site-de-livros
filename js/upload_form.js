6c3d94c32994603c7fdee13f345316bf91faaad6
Joaquim Zangui <jozangui@gmail.com>
2021-07-07 03:18:27 +0100

    update

--- js/upload_form.js
+++ js/upload_form.js
@@ -1,28 +1,47 @@
+// ========================= select tag ==============================
+
 var selectContainer, selectTag, selectedOptionDiv, optionsContainerDiv, OptionDiv;
-/*look for any elements with the class "select-container":*/
+/*
+  EN: look for any elements with the class "select-container"
+  PT: procure por algum elementos com a classe "select-container"
+*/
 selectContainer = document.getElementsByClassName("select-container");
 
 for (let i = 0; i < selectContainer.length; i++) {
   selectTag = selectContainer[i].getElementsByTagName("select")[0]; // HTML select tag: <select>
 
-  /*for each element, create a new DIV that will act as the selected item:*/
+  /*
+    EN: for each element, create a new DIV that will act as the selected item
+    PT: para cada elemento, cria uma novo DIV que atuará como o item selecionado
+  */
   selectedOptionDiv = document.createElement("DIV");
   selectedOptionDiv.setAttribute("class", "select-selected");
   selectedOptionDiv.innerHTML = selectTag.options[selectTag.selectedIndex].innerHTML;
   selectContainer[i].appendChild(selectedOptionDiv);
 
-  /*for each element, create a new DIV that will contain the option list:*/
+  /*
+    EN: for each element, create a new DIV that will contain the option list
+    PT: para cada elemento, cria uma novo DIV que conterá a lista de opções
+  */
   optionsContainerDiv = document.createElement("DIV");
   optionsContainerDiv.setAttribute("class", "select-items select-hide");
 
   for (let j = 1; j < selectTag.length; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
+    /*
+      EN: for each option in the original select element,
+    create a new DIV that will act as an option item
+      PT: para cada opção no elemento de seleção original,
+    cria um novo DIV que atuará como um item de opção
+    */
     OptionDiv = document.createElement("DIV");
     OptionDiv.innerHTML = selectTag.options[j].innerHTML;
     OptionDiv.addEventListener("click", function(e) {
-      /*when an item is clicked, update the original select box,
-      and the selected item:*/
+      /*
+        EN: when an item is clicked, update the original select box,
+      and the selected item
+        PT: quando um item é clicado, atualize a caixa de seleção original,
+      e o item selecionado
+      */
       for (let i = 0; i < selectTag.length; i++) {
         if (selectTag.options[i].innerHTML == this.innerHTML) {
           selectTag.selectedIndex = i;
@@ -53,24 +72,31 @@ for (let i = 0; i < selectContainer.length; i++) {
 function closeAllSelect(elmnt) {
   /*a function that will close all select boxes in the document,
   except the current select box:*/
-  var x, y, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (let i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
+  var optionsContainerDiv, selectedOptionDiv, arrNo = [];
+  optionsContainerDiv = document.getElementsByClassName("select-items");
+  selectedOptionDiv = document.getElementsByClassName("select-selected");
+  for (let i = 0; i < selectedOptionDiv.length; i++) {
+    if (elmnt == selectedOptionDiv[i]) {
       arrNo.push(i)
     } else {
-      y[i].classList.remove("select-arrow-active");
+      selectedOptionDiv[i].classList.remove("select-arrow-active");
     }
   }
-  for (let i = 0; i < xl; i++) {
+  for (let i = 0; i < optionsContainerDiv.length; i++) {
     if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
+      optionsContainerDiv[i].classList.add("select-hide");
     }
   }
 }
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
+
+
+// ==================================== CHANGE IMAGE ===================================
+const uploadButton = document.getElementById('upload_button');
+const img = document.getElementById('cover-image')
+
+uploadButton.addEventListener('change', function() {
+  img.src = this.value;
+});
